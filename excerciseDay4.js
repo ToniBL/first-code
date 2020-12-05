6e98339d1e2c521b149cc07b94861ef86d35d528
Anna Lene Bis-Lückenbach <a.lueckenbach@gmx.de>
2020-12-05 21:12:18 +0100

    css part 1 nearly there

--- excerciseDay4.js
+++ excerciseDay4.js
@@ -13,45 +13,49 @@ var square = new Square(4);
 square.getArea();
 
 // 1. with Prototype
-
-function Rectangle(width, height) {
-    this.width = width;
-    this.height = height;
-}
-function Square(num) {
+(function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    function Square(num) {
     this.width = num;
     this.height = num;
-}
+    }
 
 Object.prototype.getArea = function () {
     console.log(this.width * this.height);
 };
 
+}) ();
+
+
 // 2.
 // convert string into array with split
-// loop the array to find Uppercase (How?)
+// check characters case mit regular expressio A-Z
+// check character case mit regular expressions a-z https://stackoverflow.com/questions/49922278/javascript-checking-if-a-letter-is-an-uppercase
 // transform upper to lower case -> can that happen within the loop
 // store in new array
 // convert new array to string
 
-// loop string 
-// check characters case mit regular expressio A-Z
-// check character case mit regular expressions a-z https://stackoverflow.com/questions/49922278/javascript-checking-if-a-letter-is-an-uppercase
-
-function invertCase(str) {
-    for (car in str) {
-        if (car === toUpperCase)
-    }
-}
 
 
+(function() {
 
-const sentence = "das ist der Test";
-sentenceArr = sentence.split('')
-const upperCaseRegex = /[A-Z]/g; 
-for (char in sentenceArr) {
+    function invertCase(str) {
+    var test = "Hardcoded Test";
+        var strArr = test.split('');
+    console.log(strArr);}
+
+    var upperCaseRegex = /[A-Z]/g; 
+    var upper = str.match(regex);
+for ( var char in strArr) {
      if (char.match(/[A-Z]/g))
- }
+ if (char.match(upperCaseRegex)) {...}
+
+}();
 
 
- if (char.match(upperCaseRegex) {...}
+
+
+ 
