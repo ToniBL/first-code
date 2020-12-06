894850176287ae4bf724f786c99f87a859f1e595
Anna Lene Bis-Lückenbach <a.lueckenbach@gmx.de>
2020-12-06 21:34:56 +0100

    js and css completed

--- excerciseDay4.js
+++ excerciseDay4.js
@@ -19,43 +19,44 @@ square.getArea();
         this.height = height;
     }
     function Square(num) {
-    this.width = num;
-    this.height = num;
+        this.width = num;
+        this.height = num;
     }
 
-Object.prototype.getArea = function () {
-    console.log(this.width * this.height);
-};
+    Object.prototype.getArea = function () {
+        console.log(this.width * this.height);
+    };
+})();
 
-}) ();
+/* convert string into array with split
+check characters case mit regular expressio A-Z
+check character case mit regular expressions a-z https:stackoverflow.com/questions/49922278/javascript-checking-if-a-letter-is-an-uppercase
+transform upper to lower case -> can that happen within the loop
+ store in new array convert new array to string
 
-
-// 2.
-// convert string into array with split
-// check characters case mit regular expressio A-Z
-// check character case mit regular expressions a-z https://stackoverflow.com/questions/49922278/javascript-checking-if-a-letter-is-an-uppercase
-// transform upper to lower case -> can that happen within the loop
-// store in new array
-// convert new array to string
-
-
-
-(function() {
-
-    function invertCase(str) {
-    var test = "Hardcoded Test";
-        var strArr = test.split('');
+function invertCase(str) {
+           var strArr = str.split('');
     console.log(strArr);}
 
     var upperCaseRegex = /[A-Z]/g; 
     var upper = str.match(regex);
 for ( var char in strArr) {
      if (char.match(/[A-Z]/g))
- if (char.match(upperCaseRegex)) {...}
+ if (char.match(upperCaseRegex)) {...} */
 
-}();
+// 2. got the hint to compare old and new string instead of checking new string from Mikhail, he helped me solve it
 
-
-
-
- 
+function invertCase(str) {
+    var strArr = str.split("");
+    for (var i = 0; i < strArr.length; i++) {
+        if (strArr[i] === strArr[i].toUpperCase()) {
+            strArr[i] = strArr[i].toLowerCase();
+        } else if (strArr[i] === strArr[i].toLowerCase()) {
+            strArr[i] = strArr[i].toUpperCase();
+        } else {
+            continue;
+        }
+    }
+    console.log(strArr.join(""));
+    return strArr.join("");
+}
