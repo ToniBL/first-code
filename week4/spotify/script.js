fbf29fe4ba88e5b9ed8cf3a1889f375aa2f76d0d
Anna Lene Bis-Lückenbach <a.lueckenbach@gmx.de>
2020-12-27 15:31:47 +0100

    trying to break it up into functions broke the whole code :/

--- week4/spotify/script.js
+++ week4/spotify/script.js
@@ -1,9 +1,18 @@
 (function () {
-    $(".submit-button").on("click", function () {
-        var userInput = $("input").val();
-        var artistOrAlbum = $("select").val();
-        console.log("user data: ", userInput, " - ", artistOrAlbum);
+    var userInput = $("input").val();
+    var artistOrAlbum = $("select").val();
+    console.log("user data: ", userInput, " - ", artistOrAlbum);
+    var resultsHtml = "";
+    var resultsForHtml = "";
 
+    $(".submit-button").on("click", displayResults());
+    $("search-fields").on("keydown", function (e) {
+        if (e.which === 13) {
+            displayResults();
+        }
+    });
+
+    function displayResults() {
         $.ajax({
             method: "GET",
             url: "https://spicedify.herokuapp.com/spotify",
@@ -14,8 +23,6 @@
             success: function (response) {
                 response = response.artists || response.albums;
                 console.log("response: ", response);
-                var resultsHtml = "";
-                var resultsForHtml = "";
 
                 if (response.items.length === 0) {
                     console.log("no results");
@@ -52,19 +59,18 @@
                             " " +
                             userInput +
                             " </div>";
+
+                        $(".resultsFor").html(resultsForHtml);
+                        $(".results-container").html(resultsHtml);
                     }
                 }
-                $(".resultsFor").html(resultsForHtml);
-                $(".results-container").html(resultsHtml);
 
                 // We need to check whether there more results before calling replace.
-                // Calling replace on the value null will throw an error!
+                // Calling replace on the value null will throw
+                //an error!
                 // response.next part was here:
 
-               
-            },
-
- $(".more").on("click", function () {
+                $(".more").on("click", function () {
                     if (response.next === null) {
                         $(".more").css({ visibilty: "hidden" });
                     } else {
@@ -85,10 +91,47 @@
                             query: userInput,
                             type: artistOrAlbum,
                         },
+                        success: function (response) {
+                            response = response.artists || response.albums;
+                            //function displayResults(response) instead of repeating code, but how?
+                            for (var i = 0; i < response.items.length; i++) {
+                                var defaultImage =
+                                    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1PmkQ0HI-Y5Pw-SZMIOziwHaK6%26pid%3DApi&f=1";
+
+                                if (response.items[i].images.length > 0) {
+                                    defaultImage =
+                                        response.items[i].images[0].url;
+                                }
+                                resultsHtml +=
+                                    "<div>" +
+                                    "<p>" +
+                                    "<a href='" +
+                                    response.items[i].external_urls.spotify +
+                                    "'>" +
+                                    response.items[i].name +
+                                    "</a>" +
+                                    "</p>" +
+                                    "<a href='" +
+                                    response.items[i].external_urls.spotify +
+                                    "'>" +
+                                    "<img class='artist-album' src='" +
+                                    defaultImage +
+                                    "'>" +
+                                    "</a>" +
+                                    "'>" +
+                                    "</div>";
+                                resultsForHtml =
+                                    "<div>" +
+                                    "Results for" +
+                                    " " +
+                                    userInput +
+                                    " </div>";
+                            }
+                            $(".results-container").html(resultsHtml);
+                        },
                     });
                 });
-
-            
+            },
         });
-    });
+    }
 })();
